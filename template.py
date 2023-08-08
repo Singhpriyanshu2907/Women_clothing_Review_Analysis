da48ffd0922c99cf862baf308de66ef586f8e40d
Singhpriyanshu2907 <priyanshus2907@gmail.com>
2023-08-09 02:38:26 +0530

    logger & utils updated

--- template.py
+++ template.py
@@ -4,27 +4,30 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name = "Women_clothing_Review_Analysis"
-
 list_of_files = [
     ".github/workflows/.gitkeep",
-    f"src/{project_name}/__init__.py",
-    f"src/{project_name}/components/__init__.py",
-    f"src/{project_name}/utils/__init__.py",
-    f"src/{project_name}/utils/common.py",
-    f"src/{project_name}/logging/__init__.py",
-    f"src/{project_name}/config/__init__.py",
-    f"src/{project_name}/config/configuration.py",
-    f"src/{project_name}/pipeline/__init__.py",
-    f"src/{project_name}/entity/__init__.py",
-    f"src/{project_name}/constants/__init__.py",
+    f"src/__init__.py",
+    f"src/components/__init__.py",
+    f"src/components/data_ingestion.py",
+    f"src/components/data_transformation.py",
+    f"src/components/model_trainer.py",
+    f"src/utils.py",
+    f"src/autologger.py",
+    f"src/exception.py",
+    f"src/config/__init__.py",
+    f"src/config/configuration.py",
+    f"src/pipeline/__init__.py",
+    f"src/pipeline/prediction_pipeline.py",
+    f"src/pipeline/training_pipeline.py",
+    f"src/entity/__init__.py",
+    f"src/constants/__init__.py",
     "config/config.yaml",
     "params.yaml",
     "app.py",
     "main.py",
     "Dockerfile",
-    "requirements.txt",
     "setup.py",
+    f"research"
 ]
 
 
